e92e5528fda371fc8b6f663d22329b2635598ffb
Yevhenii Pozniak <jekamanu@gmail.com>
2024-07-04 20:31:17 +0300

    update Header styles

--- src/App.module.js
+++ src/App.module.js
@@ -2,16 +2,14 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 960px;
   margin: 0 auto;
-  padding: 0 16px;
+  padding: 0 50px;
 `;
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  gap: 12px;
+  justify-content: center;
   padding: 8px 0;
   margin-bottom: 16px;
   border-bottom: 1px solid black;
@@ -32,9 +30,10 @@ export const Link = styled(NavLink)`
   text-decoration: none;
   color: black;
   font-weight: 500;
+  margin-left: 35px;
 
   &.active {
     color: white;
-    background-color: orangered;
+    background-color: #e44848;
   }
 `;
