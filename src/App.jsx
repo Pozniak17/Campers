61e54c52cb4f34b987e1f81f31e095ac403f6c4e
Yevhenii Pozniak <jekamanu@gmail.com>
2024-07-04 09:52:39 +0300

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import CardList from "./components/Cards/CardList/CardList";
 import FilterList from "./components/Filters/FilterList/FilterList";
 import { Layout } from "./components/Layout/Layout.module";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        "https://6602f96b9d7276a75554be5d.mockapi.io/advert"
+      );
+      console.log(response.data);
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       <FilterList />
-      <CardList />
+      <CardList items={data} />
     </Layout>
   );
 }
