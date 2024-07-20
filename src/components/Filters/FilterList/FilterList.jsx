568c58e9f7e1e95367ba7589102fe35c0e066c54
Yevhenii Pozniak <jekamanu@gmail.com>
2024-07-20 13:24:54 +0300

    Update FilterList.jsx

--- src/components/Filters/FilterList/FilterList.jsx
+++ src/components/Filters/FilterList/FilterList.jsx
@@ -1,22 +1,36 @@
+/* eslint-disable react/prop-types */
 import { Form, Formik } from "formik";
 import ButtonFilter from "../ButtonFilter/ButtonFilter";
-import EquipmentFilter from "../EquipmentFilter/EquipmentFilter";
+// import EquipmentFilter from "../EquipmentFilter/EquipmentFilter";
+// import TypeFilter from "../TypeFilter/TypeFilter";
 import LocationFilter from "../LocationFilter/LocationFilter";
-import TypeFilter from "../TypeFilter/TypeFilter";
 import { Wrapper } from "./FilterList.module";
 
-export default function FilterList() {
+export default function FilterList({ items }) {
   const initialValues = {
     location: "",
+    // airConditioner: false,
+    // transmission: true,
+    // kitchen: false,
+    // TV: false,
+    // shower: false,
+  };
+
+  const handleSubmit = (values) => {
+    const visibleTasks = items.filter((item) =>
+      item.location.toLowerCase().includes(values.toLowerCase())
+    );
+
+    console.log(visibleTasks);
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={() => {}}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <Form>
         <Wrapper>
           <LocationFilter />
-          <EquipmentFilter />
-          <TypeFilter />
+          {/* <EquipmentFilter />
+          <TypeFilter /> */}
           <ButtonFilter />
         </Wrapper>
       </Form>
